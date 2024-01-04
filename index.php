3d02408ec523232955b66f4e21aa2b6ffe9c7712
amatjkay <amatjkay@gmail.com>
2024-01-04 20:39:43 +0300

    test

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP learning</title>
 </head>
 
 <body>
-    <?php
+<?php
 
-    echo "HI MIR"
 
-    ?>
+
+?>
 </body>
 
 </html>
